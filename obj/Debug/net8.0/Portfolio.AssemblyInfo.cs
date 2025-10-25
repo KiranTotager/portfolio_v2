f4bb366ce3b27dcaea44783d2d137d6c8fb4733d
KiranTotager <tskiran5114@gmail.com>
2025-10-25 18:12:40 +0530

    evening commi

--- obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+241089a93bebf580d1996344d66fc29cd401bfdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1e6f79c8e365a618f2da2a3bfec34ddb4077458")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
