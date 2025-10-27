c006987b25f6cdd55622381f98e285932a7366d4
KiranTotager <tskiran5114@gmail.com>
2025-10-27 08:55:23 +0530

    token generation

---
+++ Models/RefreshToken.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Models
+{
+    public class RefreshToken
+    {
+        [Required]
+        [BsonElement("user_id")]
+        public string UserId { get; set; }
+        [Required]
+        [BsonElement("Refresh_token")]
+        public string Token { get; set; }
+        [Required]
+        [BsonElement("expiry_time")]
+        public DateTime ExpiryTime { get; set; }
+    }
+}
--- Utils/TokenService.cs
+++ Utils/TokenService.cs
@@ -2,6 +2,8 @@
 using Portfolio.Dto.RequestDto;
 using Portfolio.Interfaces.IUtils;
 using Portfolio.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -17,12 +19,29 @@ namespace Portfolio.Utils
             int ExpiryTime = _configuration.GetValue<int>("Jwt:AuthTokenExpiryTime");
             var SecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetValue<string>("Jwt:SecretKey")));
             var Credentials = new SigningCredentials(SecurityKey, SecurityAlgorithms.HmacSha256);
-            throw new NotImplementedException();
+            var userClaims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier,applicationUser.Id),
+                new Claim(ClaimTypes.Role,applicationUser.Role.ToString()),
+            };
+            var Token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                signingCredentials: Credentials,
+                claims: userClaims,
+                expires: DateTime.Now.AddMinutes(ExpiryTime)
+                );
+            return new JwtSecurityTokenHandler().WriteToken(Token);
         }
 
         public string GenerateRefreshToken()
         {
-            throw new NotImplementedException();
+            var RandomNumber=new byte[32];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(RandomNumber);
+                return Convert.ToBase64String(RandomNumber);
+            }
         }
     }
 }
